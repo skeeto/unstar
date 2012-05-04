122168e7fbf8faedc52854f6bc0ce82be8bdf395
Christopher Wellons <mosquitopsu@gmail.com>
2012-05-03 20:27:29 -0400

    Mark out the special test.

--- test/unstar/CrackerTest.java
+++ test/unstar/CrackerTest.java
@@ -22,7 +22,7 @@ public class CrackerTest {
     public void serialTest() {
         /* Public */
         assertEquals(0x1009db6241eL, Cracker.decode("E2BO659A"));
-        assertEquals(0x10120a6bad5L, Cracker.decode("E3C03B2T"));
+        assertEquals(0x10120a6bad5L, Cracker.decode("E3C03B2T")); // personal
         assertEquals(0x101e22fa47cL, Cracker.decode("E4TP99BW"));
         assertEquals(0x105d81aa6a9L, Cracker.decode("ECN101WP"));
         assertEquals(0x1071daf128cL, Cracker.decode("EF5D4KNG"));
