56f212de965bf0eb89410f4ed187b40d9b6417e3
Christopher Wellons <mosquitopsu@gmail.com>
2012-05-05 22:42:03 -0400

    Add a terminator line to InputStreamCodeProvider.

--- src/unstar/InputStreamCodeProvider.java
+++ src/unstar/InputStreamCodeProvider.java
@@ -1,9 +1,8 @@
 package unstar;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 
@@ -13,11 +12,17 @@ import lombok.extern.java.Log;
 @Log
 public final class InputStreamCodeProvider implements CodeProvider {
 
-    /** The file reader for reading codes. */
-    private final BufferedReader reader;
+    /** The terminator line that can end the stream early. */
+    public static final String TERMINATOR = "--------";
 
-    /** The next code. */
-    private String next = null;
+    /** The file reader for reading codes. */
+    private final Scanner scanner;
+
+    /** Peekahead string for checking for the terminator. */
+    private String peek = null;
+
+    /** True if the terminator line has been read. */
+    private boolean terminated = false;
 
     /**
      * Create a new provider for the given file.
@@ -25,29 +30,41 @@ public final class InputStreamCodeProvider implements CodeProvider {
      */
     @SneakyThrows
     public InputStreamCodeProvider(final InputStream in) {
-        reader = new BufferedReader(new InputStreamReader(in));
-        next = reader.readLine();
+        scanner = new Scanner(in);
     }
 
     @Override
     @SneakyThrows
     public String next() {
-        String ret = next;
-        next = reader.readLine();
-        if (ret == null) {
+        if (terminated) {
+            throw new NoSuchElementException();
+        } else if (peek != null) {
+            String ret = peek;
+            peek = null;
+            return ret;
+        } else if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return ret;
+        return next();
     }
 
     @Override
     public boolean hasNext() {
-        return next != null;
+        if (peek != null) {
+            return true;
+        } else if (scanner.hasNextLine()) {
+            peek = scanner.nextLine();
+            terminated = peek.equals(TERMINATOR);
+            return !terminated;
+        } else {
+            terminated = true;
+            return false;
+        }
     }
 
     @Override
     public void report(final String code) {
-        log.info("valid: " + code);
+        log.info("VALID " + code);
     }
 
     @Override
